3481a6f6ef5f297efe6e70479cec4bd873e80e09
walssimon <walssimon123@gmail.com>
2023-04-18 20:25:59 -0300

    editando login

--- app/src/main/java/br/com/etecia/maufuturo/LoginActivity.java
+++ app/src/main/java/br/com/etecia/maufuturo/LoginActivity.java
@@ -3,9 +3,13 @@ package br.com.etecia.maufuturo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+ EditText login, senha;
 
+Button btnentrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
